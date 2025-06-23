4a0319d7107cdb6e8047c43bd663582b5947425a
Vitor gabriel <vitorgabrieltrabalho@gmail.com>
2025-06-23 14:01:59 -0300

    atualização

--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -6,4 +6,8 @@ class Jogo {
     var capa = ""
     val descricao  = ""
 
+    override fun toString(): String {
+        return "Meu Jogo= '\n" + "Titulo: $titulo'\n, Capa='$capa'\n , Descricao='$descricao')"
+    }
+
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,17 +7,19 @@ import java.net.http.HttpResponse
 
 
 fun main() {
-   val client: HttpClient = HttpClient.newHttpClient()
-       val  request = HttpRequest.newBuilder()
+    val client: HttpClient = HttpClient.newHttpClient()
+    val  request = HttpRequest.newBuilder()
         .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
         .build()
 
 
     val response = client
         .send(request, HttpResponse.BodyHandlers.ofString())
-   val json = response.body()
+    val json = response.body()
     println(json)
 
-  val meuJogo = Jogo()
-
+    val meuJogo = Jogo()
+    meuJogo.titulo = "Batman: Arkham Asylum Game of the Year Edition"
+    meuJogo.capa = "https:\\/\\/cdn.cloudflare.steamstatic.com\\/steam\\/apps\\/35140\\/capsule_sm_120.jpg?t=1681938587"
+    println(meuJogo)
 }
